2ccaf0413c6bd9b907bc2f468e0be256e8f998e3
Roniel Bernas <roniel.bernas@crmworksa.asia>
2017-06-12 12:09:05 +0800

    built project using create-react-ap added header component for the app added header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './oction-logo-header.png';
+
+const Header = () => {
+
+    return (
+        <div className="App-header">
+            <img src={logo} alt="logo" />
+            <h2>Welcome to Oction</h2>
+        </div>
+    )
+}
+
+export default Header
